1073562be5c728380bb3ce2590081064b1acf100
joaosousafranco <joao.franco@jsfsi.com>
2022-08-23 14:07:48 +0100

    1 file modified, 1 deleted

--- declarations.d.ts
+++
@@ -1,9 +0,0 @@
-type DefaultCanvasJSReact = {
-  CanvasJSChart: any;
-};
-
-declare module 'canvasjs-react-charts' {
-  const CanvasJSReact: DefaultCanvasJSReact;
-
-  export default CanvasJSReact;
-}
