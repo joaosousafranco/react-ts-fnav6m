665ea347a4b3b03663483c59b002d27f561c46b3
joaosousafranco <joao.franco@jsfsi.com>
2022-08-23 11:22:40 +0100

    2 files modified

--- ui/app/App.tsx
+++ ui/app/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { AppProvider } from './AppProvider';
 import ErrorBoundary from './ErrorBoundary';
 import { Navigator } from './Navigator';
+import cx from 'classnames';
 import './App.style';
 
 const App = () => {
@@ -9,6 +10,12 @@ const App = () => {
     <ErrorBoundary onError={(error) => console.log(JSON.stringify(error))}>
       <AppProvider>
         <Navigator />
+        <div className={cx('footer')}>
+          <div>Code at:</div>
+          <a href="https://github.com/joaosousafranco/react-ts-fnav6m">
+            https://github.com/joaosousafranco/react-ts-fnav6m
+          </a>
+        </div>
       </AppProvider>
     </ErrorBoundary>
   );
